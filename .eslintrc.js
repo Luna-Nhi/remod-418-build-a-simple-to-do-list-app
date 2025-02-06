b48b27068fbdc6083e468f2cbf83542815f8a110
Luna - Tuyết Nhi <nhtuyetnhi2609@gmail.com>
2025-02-07 01:52:17 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
+// filepath: /c:/Users/TUYET NHI/Documents/GitHub/remod-418-build-a-simple-to-do-list-app/.eslintrc.js
 module.exports = {
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react-refresh/recommended', // Thêm dòng này
-    ],
-    plugins: [
-      'react',
-      '@typescript-eslint',
-      'react-refresh', // Thêm dòng này
-    ],
-    rules: {
-      // Các quy tắc khác của bạn
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:react-refresh/recommended', // Xóa dòng này nếu không cần thiết
+  ],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    // 'react-refresh', // Xóa dòng này nếu không cần thiết
+  ],
+  rules: {
+    // Xóa quy tắc 'react-refresh/only-export-components' nếu nó tồn tại
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  };
+  },
+};
